fedab3af6fc5ab3dfa6aa07e45b8ce3df721aa86
edmarques11 <edmarquessantosdelima@gmail.com>
2020-07-29 15:20:28 -0300

    Updates

---
+++ precache-manifest.0eb95571296fb4ce1c234d9acae22737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19de2314be9aa427d69955352a852f56",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "eaf46c0bf5ad7c20540e",
+    "url": "/Calculator/static/css/main.00c38915.chunk.css"
+  },
+  {
+    "revision": "0bd1f2da68aff65f190e",
+    "url": "/Calculator/static/js/2.2b43ec4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.2b43ec4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf46c0bf5ad7c20540e",
+    "url": "/Calculator/static/js/main.11c73bdb.chunk.js"
+  },
+  {
+    "revision": "7243594aa9520a60bfcb",
+    "url": "/Calculator/static/js/runtime-main.16de0090.js"
+  },
+  {
+    "revision": "f444b5a59ecb3f931f8ce7e404957aec",
+    "url": "/Calculator/static/media/RobotoMono-Thin.f444b5a5.ttf"
+  }
+]);
